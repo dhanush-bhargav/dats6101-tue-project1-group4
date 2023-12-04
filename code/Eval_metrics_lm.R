eba930c6421e0e8be649e8aa263d653946a115dc
pavanichella12 <pavani.chella@gwu.edu>
2023-12-04 00:47:53 -0500

    updatone

--- code/Eval_metrics_lm.R
+++ code/Eval_metrics_lm.R
@@ -24,3 +24,19 @@ n <- nrow(test_data)
 p <- length(coef(lm_model_new)) - 1  # Number of predictors
 adj_r_squared <- 1 - ((1 - r_squared) * (n - 1) / (n - p - 1))
 cat("Adjusted R-squared:", adj_r_squared, "\n")
+
+#evaluation metrics for random forest
+
+mae_rf <- mean(abs(predictions_rf - test_data$years_of_experience))
+cat("Random Forest Mean Absolute Error (MAE):", mae_rf, "\n")
+
+
+mse_rf <- mean((predictions_rf - test_data$years_of_experience)^2)
+cat("Random Forest Mean Squared Error (MSE):", mse_rf, "\n")
+
+
+
+
+
+
+
